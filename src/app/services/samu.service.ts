0b9987bdc270688aedf79d65a43a65d3703b7416
johnny <jonathan99moura@hotmail.com>
2017-07-17 11:18:46 -0300

    segue esta porra

--- src/app/services/samu.service.ts
+++ src/app/services/samu.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { UF } from '../types/uf';
+
 import { Dados } from '../types/samu';
 import { VALORES } from './mock-samu_municipios_atendidos_por_estado';
 
 @Injectable()
 export class SamuService {
-
   getAllMunicipiosAtendidosPorEstado(): Dados[] {
     return VALORES;
   }
 
-  getoMunicipiosAtendidosDoEstado(uf_id: number): Dados[] {
-    var uf_atendidos: Dados[] = [];
+  getPorUFMunicipiosAtendidosPorEstado(uf: UF): Dados[]{
+    var total : Dados[] = [];
     for(let mun of VALORES){
-      if(mun.uf_id == uf_id) uf_atendidos.push(mun);
+      if(mun.uf_id == uf.id) total.push(mun);
     }
-    return uf_atendidos;
+    return total;
   }
 }
